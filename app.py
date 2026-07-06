44a18b162880b55705806d2266473bea45c63209
FranzCod3r <francis_coder@outlook.com>
2026-07-06 10:11:01 +0200

    added emoji

--- app.py
+++ app.py
@@ -47,8 +47,8 @@ def load_data(path):
 # MAIN APP
 # --------------------------------------------
 
-st.markdown("<h1 style='text-align: center;'>Dashboard Vendite Avanzata</h1>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: center;'>Analisi completa: vendite, profitti, logistica, prodotti e mappa geografica</p>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>📊 Dashboard Vendite Avanzata</h1>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center;'>🎯 Analisi completa: vendite, profitti, logistica, prodotti e mappa geografica</p>", unsafe_allow_html=True)
 st.markdown("""
 <div style='text-align: left;'>
     <p style='font-weight: 600;'>Per iniziare: 👇</p>
